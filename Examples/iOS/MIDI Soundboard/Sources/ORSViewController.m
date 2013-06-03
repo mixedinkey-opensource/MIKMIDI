383085fa421d36708b0a9ded1a58c8370a5c9beb
Andrew Madsen <andrew@openreelsoftware.com>
2013-06-02 18:31:32 -0600

    Added MIDI Soundboard example app for iOS.

---
+++ Examples/iOS/MIDI Soundboard/Sources/ORSViewController.m
@@ -0,0 +1,29 @@
+//
+//  ORSViewController.m
+//  MIDI Soundboard
+//
+//  Created by Andrew Madsen on 6/2/13.
+//  Copyright (c) 2013 Open Reel Software. All rights reserved.
+//
+
+#import "ORSViewController.h"
+
+@interface ORSViewController ()
+
+@end
+
+@implementation ORSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
