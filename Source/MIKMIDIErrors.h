594fc4711cac88c354b42154c1b535721208192e
Andrew Madsen <andrew@openreelsoftware.com>
2013-07-29 14:29:39 -0600

    Added MIKMIDIErrors for MIKMIDI error handling.

---
+++ Source/MIKMIDIErrors.h
@@ -0,0 +1,22 @@
+//
+//  MIKMIDIErrors.h
+//  Danceability
+//
+//  Created by Andrew Madsen on 7/19/13.
+//  Copyright (c) 2013 Mixed In Key. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static NSString * const MIKMIDIErrorDomain;
+
+typedef NS_ENUM(NSInteger, MIKMIDIErrorCode) {
+	MIKMIDIUnknownErrorCode = 1,
+	MIKMIDIDeviceConnectionLostErrorCode,
+};
+
+@interface NSError (MIKMIDI)
+
++ (instancetype)MIKMIDIErrorWithCode:(NSInteger)code userInfo:(NSDictionary *)userInfo;
+
+@end
