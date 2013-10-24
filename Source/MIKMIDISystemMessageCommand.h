e74b9473ff316cfa5956934acb582c4fd6f1ed11
Andrew Madsen <andrew@openreelsoftware.com>
2013-10-24 16:50:16 -0600

    Added readwrite property declarations to MIKMutableMIDISystemMessageCommand.

--- Source/MIKMIDISystemMessageCommand.h
+++ Source/MIKMIDISystemMessageCommand.h
@@ -14,4 +14,12 @@
 
 @interface MIKMutableMIDISystemMessageCommand : MIKMIDISystemMessageCommand
 
+@property (nonatomic, strong, readwrite) NSDate *timestamp;
+@property (nonatomic, readwrite) MIKMIDICommandType commandType;
+@property (nonatomic, readwrite) UInt8 dataByte1;
+@property (nonatomic, readwrite) UInt8 dataByte2;
+
+@property (nonatomic, readwrite) MIDITimeStamp midiTimestamp;
+@property (nonatomic, copy, readwrite) NSData *data;
+
 @end
