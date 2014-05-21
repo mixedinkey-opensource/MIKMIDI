ca542e86f74251a8e45db9e11b7367494a5f215a
Jake Gundersen <fattjake@gmail.com>
2014-05-21 15:06:01 -0600

    Working on parsing tracks

---
+++ Source/MIKMIDIEvent.m
@@ -0,0 +1,13 @@
+//
+//  MIKMIDIEvent.m
+//  MIDI Files Testbed
+//
+//  Created by Jake Gundersen on 5/21/14.
+//  Copyright (c) 2014 Mixed In Key. All rights reserved.
+//
+
+#import "MIKMIDIEvent.h"
+
+@implementation MIKMIDIEvent
+
+@end
