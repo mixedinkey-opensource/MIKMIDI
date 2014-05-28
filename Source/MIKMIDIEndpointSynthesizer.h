df8c0e16b44f56f3c7cbfcf1cda784e3952bcd75
Andrew Madsen <andrew@openreelsoftware.com>
2014-05-27 20:06:52 -0600

    Added MIKMIDIEndpointSynthesizer class to play sound from MIDI device input.

---
+++ Source/MIKMIDIEndpointSynthesizer.h
@@ -0,0 +1,21 @@
+//
+//  MIKMIDIPlayer.h
+//  MIDI Player
+//
+//  Created by Andrew Madsen on 5/27/14.
+//  Copyright (c) 2014 Mixed In Key. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class MIKMIDISourceEndpoint;
+
+@interface MIKMIDIEndpointSynthesizer : NSObject
+
++ (instancetype)playerWithMIDISource:(MIKMIDISourceEndpoint *)source;
+
+- (instancetype)initWithMIDISource:(MIKMIDISourceEndpoint *)source;
+
+@property (nonatomic, strong, readonly) MIKMIDISourceEndpoint *source;
+
+@end
