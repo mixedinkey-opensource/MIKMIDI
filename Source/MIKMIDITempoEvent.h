0996e7df9aa69ffb8c808344fb194f6f1b457589
Chris Flesner <chris@mixedinkey.com>
2014-11-20 15:03:24 -0600

    Documented MIKMIDITempoEvent

--- Source/MIKMIDITempoEvent.h
+++ Source/MIKMIDITempoEvent.h
@@ -8,14 +8,34 @@
 
 #import "MIKMIDIEvent.h"
 
+/** 
+ *  A MIDI tempo event.
+ */
 @interface MIKMIDITempoEvent : MIKMIDIEvent
 
+/**
+ *  The beats per minute of the tempo event.
+ */
 @property (nonatomic, readonly) Float64 bpm;
 
+
+/**
+ *  Creates and initializes a new MIKMIDITempoEvent.
+ *
+ *  @param timeStamp The time stamp for the tempo event.
+ *
+ *  @param bpm The beats per minute of the tempo event.
+ *
+ *  @return A new instance of MIKMIDITempoEvent, or nil if an error occured.
+ */
 + (instancetype)tempoEventWithTimeStamp:(MusicTimeStamp)timeStamp tempo:(Float64)bpm;
 
 @end
 
+
+/**
+ *  The mutable counterpart of MIKMIDITempoEvent.
+ */
 @interface MIKMutableMIDITempoEvent : MIKMIDITempoEvent
 
 @property (nonatomic, readwrite) Float64 bpm;
