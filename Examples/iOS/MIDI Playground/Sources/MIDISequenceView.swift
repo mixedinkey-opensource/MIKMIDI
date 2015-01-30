74724abcdd1fbb572209d6d9a81a7eeec277d38a
Andrew Madsen <andrew@openreelsoftware.com>
2015-01-29 19:15:22 -0700

    Created MIDI Playground iOS example project.

---
+++ Examples/iOS/MIDI Playground/Sources/MIDISequenceView.swift
@@ -0,0 +1,14 @@
+//
+//  MIDISequenceView.swift
+//  MIDI Playground
+//
+//  Created by Andrew Madsen on 1/29/15.
+//  Copyright (c) 2015 Mixed In Key. All rights reserved.
+//
+
+import UIKit
+import MIKMIDI
+
+class MIDISequenceView : UIView {
+   
+}
---
+++ Examples/iOS/MIDI Playground/Sources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MIDI Playground
+//
+//  Created by Andrew Madsen on 1/29/15.
+//  Copyright (c) 2015 Mixed In Key. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
