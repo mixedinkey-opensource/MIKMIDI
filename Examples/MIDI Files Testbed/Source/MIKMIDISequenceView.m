edb387d07d726d92f34a27a5287593b2ebed598c
Andrew Madsen <andrew@mixedinkey.com>
2015-03-04 16:05:51 -0700

    MIDI Files Testbed: Fixed MIKMIDISequence view's note coloring for sequences with exactly 2 note tracks.

--- Examples/MIDI Files Testbed/Source/MIKMIDISequenceView.m
+++ Examples/MIDI Files Testbed/Source/MIKMIDISequenceView.m
@@ -39,7 +39,7 @@
 	for (MIKMIDITrack *track in self.sequence.tracks) {
 		
 		for (MIKMIDINoteEvent *note in [track notes]) {
-			NSColor *noteColor = [self.sequence.tracks count] <= 2 ? [self colorForNote:note] : [self colorForTrackAtIndex:index];
+			NSColor *noteColor = [self.sequence.tracks count] < 2 ? [self colorForNote:note] : [self colorForTrackAtIndex:index];
 			
 			[[NSColor blackColor] setStroke];
 			[noteColor setFill];
