89a7b5d037830c1c4867398691e8e2d590f8f797
Andrew Madsen <andrew@mixedinkey.com>
2015-03-04 10:44:23 -0700

    Issue #63: Added MIKMIDIPolyphonicKeyPressureEvent.

---
+++ Source/MIKMIDIPolyphonicKeyPressureEvent.m
@@ -0,0 +1,84 @@
+//
+//  MIKMIDIPolyphonicKeyPressureEvent.m
+//  MIKMIDI
+//
+//  Created by Andrew Madsen on 3/4/15.
+//  Copyright (c) 2015 Mixed In Key. All rights reserved.
+//
+
+#import "MIKMIDIPolyphonicKeyPressureEvent.h"
+#import "MIKMIDIEvent_SubclassMethods.h"
+
+#if !__has_feature(objc_arc)
+#error MIKMIDIPolyphonicKeyPressureEvent.m must be compiled with ARC. Either turn on ARC for the project or set the -fobjc-arc flag for MIKMIDIPolyphonicKeyPressureEvent.m in the Build Phases for this target
+#endif
+
+@interface MIKMIDIChannelEvent (Protected)
+
+@property (nonatomic, readwrite) UInt8 channel;
+@property (nonatomic, readwrite) UInt8 dataByte1;
+@property (nonatomic, readwrite) UInt8 dataByte2;
+
+@end
+
+@interface MIKMIDIPolyphonicKeyPressureEvent ()
+
+@property (nonatomic, readwrite) UInt8 note;
+@property (nonatomic, readwrite) UInt8 pressure;
+
+@end
+
+@implementation MIKMIDIPolyphonicKeyPressureEvent
+
++ (void)load { [MIKMIDIEvent registerSubclass:self]; }
++ (NSArray *)supportedMIDIEventTypes { return @[@(MIKMIDIEventTypeMIDIPolyphonicKeyPressureMessage)]; }
++ (Class)immutableCounterpartClass { return [MIKMIDIPolyphonicKeyPressureEvent class]; }
++ (Class)mutableCounterpartClass { return [MIKMutableMIDIPolyphonicKeyPressureEvent class]; }
++ (BOOL)isMutable { return NO; }
+
++ (NSSet *)keyPathsForValuesAffectingNote
+{
+	return [NSSet setWithObjects:@"dataByte1", nil];
+}
+
+- (UInt8)note
+{
+	return self.dataByte1;
+}
+
+- (void)setNote:(UInt8)note
+{
+	self.dataByte1 = MIN(note, 127);
+}
+
++ (NSSet *)keyPathsForValuesAffectingPressure
+{
+	return [NSSet setWithObjects:@"dataByte2", nil];
+}
+
+- (UInt8)pressure
+{
+	return self.dataByte2;
+}
+
+- (void)setPressure:(UInt8)pressure
+{
+	self.dataByte2 = MIN(pressure, 127);
+}
+
+@end
+
+@implementation MIKMutableMIDIPolyphonicKeyPressureEvent
+
+@dynamic note;
+@dynamic pressure;
+
+@dynamic timeStamp;
+@dynamic data;
+@dynamic channel;
+@dynamic dataByte1;
+@dynamic dataByte2;
+
++ (BOOL)isMutable { return YES; }
+
+@end
