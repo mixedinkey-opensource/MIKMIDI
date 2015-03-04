eeb61031db13cc3aa16b31c656c430ae0fc82940
Andrew Madsen <andrew@mixedinkey.com>
2015-03-04 11:18:27 -0700

    Issue #63: Added MIKMIDIPitchBendChangeEvent.

---
+++ Source/MIKMIDIPitchBendChangeEvent.h
@@ -0,0 +1,41 @@
+//
+//  MIKMIDIPitchBendChangeEvent.h
+//  MIKMIDI
+//
+//  Created by Andrew Madsen on 3/4/15.
+//  Copyright (c) 2015 Mixed In Key. All rights reserved.
+//
+
+#import "MIKMIDIChannelEvent.h"
+
+/**
+ *  A pitch bed change event.
+ *
+ *  This event indicates a pitch bend change. On devices, pitch
+ *  bends are usually generated using a wheel or lever.
+ */
+@interface MIKMIDIPitchBendChangeEvent : MIKMIDIChannelEvent
+
+/**
+ *  A 14-bit value indicating the pitch bend.
+ *  Center is 0x2000 (8192). 
+ *  Valid range is from 0-16383.
+ */
+@property (nonatomic, readonly) UInt16 pitchChange;
+
+@end
+
+/**
+ *  The mutable counterpart of MIKMIDIPitchBendChangeEvent.
+ */
+@interface MIKMutableMIDIPitchBendChangeEvent : MIKMIDIPitchBendChangeEvent
+
+@property (nonatomic, readonly) UInt16 pitchChange;
+
+@property (nonatomic, readwrite) MusicTimeStamp timeStamp;
+@property (nonatomic, strong, readwrite) NSMutableData *data;
+@property (nonatomic, readwrite) UInt8 channel;
+@property (nonatomic, readwrite) UInt8 dataByte1;
+@property (nonatomic, readwrite) UInt8 dataByte2;
+
+@end
