cc127f8dbd38866e5198e5f380cc2c3d29ed31d3
Andrew Madsen <andrew@mixedinkey.com>
2015-03-04 10:12:44 -0700

    MIDI Files Testbed: Let the run loop spin a little after accepting a drop to give the drop a chance to finish. Aids debugging.

--- Examples/MIDI Files Testbed/Source/MIKMainWindowController.m
+++ Examples/MIDI Files Testbed/Source/MIKMainWindowController.m
@@ -79,7 +79,9 @@
 
 - (void)midiSequenceView:(MIKMIDISequenceView *)sequenceView receivedDroppedMIDIFiles:(NSArray *)midiFiles
 {
-	[self loadMIDIFile:[midiFiles firstObject]];
+	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+		[self loadMIDIFile:[midiFiles firstObject]];
+	});
 }
 
 #pragma mark - Private
