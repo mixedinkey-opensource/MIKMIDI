mixedinkey-opensource/MIKMIDI

commit 6b13587bfa1b21f134f554b48f3302869c57313f

Issue #48: MIKMIDISequencer playback no longer stalls when mouse is held down.

Source/MIKMIDISequencer.m
@@ -140,11 +140,12 @@
 	self.pendingNoteOffs = [NSMutableDictionary dictionary];
 	self.pendingNoteOffMIDITimeStamps = [NSMutableOrderedSet orderedSet];
 	self.lastProcessedMIDITimeStamp = midiTimeStamp - 1;
-	self.processingTimer = [NSTimer scheduledTimerWithTimeInterval:0.05
-															target:self
-														  selector:@selector(processingTimerFired:)
-														  userInfo:nil
-														   repeats:YES];
+	NSTimer *processingTimer = [NSTimer timerWithTimeInterval:0.05
+													   target:self
+													 selector:@selector(processingTimerFired:)
+													 userInfo:nil
+													  repeats:YES];
+	[[NSRunLoop currentRunLoop] addTimer:processingTimer forMode:NSRunLoopCommonModes];
 	[self.processingTimer fire];
 }
 

commit 962faa1e9eb08928a5b1698fc21e25689a46fe11

Fixed MIKMIDISequencer's failure to stop when -stop is called. Broken in 6b13587 (Issue #48).

Source/MIKMIDISequencer.m
@@ -140,12 +140,11 @@
 	self.pendingNoteOffs = [NSMutableDictionary dictionary];
 	self.pendingNoteOffMIDITimeStamps = [NSMutableOrderedSet orderedSet];
 	self.lastProcessedMIDITimeStamp = midiTimeStamp - 1;
-	NSTimer *processingTimer = [NSTimer timerWithTimeInterval:0.05
+	self.processingTimer = [NSTimer timerWithTimeInterval:0.05
 													   target:self
 													 selector:@selector(processingTimerFired:)
 													 userInfo:nil
 													  repeats:YES];
-	[[NSRunLoop currentRunLoop] addTimer:processingTimer forMode:NSRunLoopCommonModes];
 	[self.processingTimer fire];
 }
 
@@ -610,6 +609,7 @@
 	if (processingTimer != _processingTimer) {
 		[_processingTimer invalidate];
 		_processingTimer = processingTimer;
+		if (_processingTimer) [[NSRunLoop currentRunLoop] addTimer:_processingTimer forMode:NSRunLoopCommonModes];
 	}
 }
 