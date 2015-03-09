e29e10b9d9c87c7efed4e14d901b9f326b726ab0
Andrew Madsen <andrew@mixedinkey.com>
2015-03-09 16:45:34 -0600

    Issue #35: Added unit tests for -[MIKMIDITrack moveEventsFromStartingTimeStamp:...].

--- Framework/MIKMIDI Tests/MIKMIDITrackTests.m
+++ Framework/MIKMIDI Tests/MIKMIDITrackTests.m
@@ -77,6 +77,170 @@
 	[track removeObserver:self forKeyPath:@"events"];
 }
 
+- (void)testMovingSingleEvent
+{
+	MIKMIDISequence *sequence = [MIKMIDISequence sequence];
+	MIKMIDITrack *track = [sequence addTrack];
+	MIKMIDIEvent *event1 = [MIKMIDINoteEvent noteEventWithTimeStamp:1 note:60 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event2 = [MIKMIDINoteEvent noteEventWithTimeStamp:2 note:61 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event3 = [MIKMIDINoteEvent noteEventWithTimeStamp:4 note:62 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event4 = [MIKMIDINoteEvent noteEventWithTimeStamp:6 note:63 velocity:127 duration:1 channel:0];
+	NSArray *allEvents = @[event1, event2, event3, event4];
+	[track addEvents:allEvents];
+	
+	self.eventsChangeNotificationReceived = NO;
+	[track addObserver:self forKeyPath:@"events" options:0 context:NULL];
+	{
+		// Move event 2 to timestamp 5
+		[track moveEventsFromStartingTimeStamp:2 toEndingTimeStamp:2 byAmount:3];
+		XCTAssertTrue(self.eventsChangeNotificationReceived, @"Moving an event in MIKMIDITrack did not produce a KVO notification.");
+		MIKMIDIEvent *expectedEvent2AfterMove = [MIKMIDINoteEvent noteEventWithTimeStamp:5 note:61 velocity:127 duration:1 channel:0];
+		NSArray *expectedNewEvents = @[event1, event3, expectedEvent2AfterMove, event4];
+		XCTAssertEqualObjects(track.events, expectedNewEvents, @"Moving an event in MIKMIDITrack failed.");
+	}
+	[track removeObserver:self forKeyPath:@"events"];
+}
+
+- (void)testMovingMultipleEventsAtSameTimestamp
+{
+	MIKMIDISequence *sequence = [MIKMIDISequence sequence];
+	MIKMIDITrack *track = [sequence addTrack];
+	MIKMIDIEvent *event1 = [MIKMIDINoteEvent noteEventWithTimeStamp:1 note:60 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event2 = [MIKMIDINoteEvent noteEventWithTimeStamp:2 note:61 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event3 = [MIKMIDINoteEvent noteEventWithTimeStamp:2 note:62 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event4 = [MIKMIDINoteEvent noteEventWithTimeStamp:3 note:63 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event5 = [MIKMIDINoteEvent noteEventWithTimeStamp:6 note:64 velocity:127 duration:1 channel:0];
+	NSArray *allEvents = @[event1, event2, event3, event4, event5];
+	[track addEvents:allEvents];
+	
+	self.eventsChangeNotificationReceived = NO;
+	[track addObserver:self forKeyPath:@"events" options:0 context:NULL];
+	{
+		// Move event 2 to timestamp 5
+		[track moveEventsFromStartingTimeStamp:2 toEndingTimeStamp:2 byAmount:3];
+		XCTAssertTrue(self.eventsChangeNotificationReceived, @"Moving events in MIKMIDITrack did not produce a KVO notification.");
+		MIKMIDIEvent *expectedEvent2AfterMove = [MIKMIDINoteEvent noteEventWithTimeStamp:5 note:61 velocity:127 duration:1 channel:0];
+		MIKMIDIEvent *expectedEvent3AfterMove = [MIKMIDINoteEvent noteEventWithTimeStamp:5 note:62 velocity:127 duration:1 channel:0];
+		
+		// Use sets, because order of events with the same timestamp is (acceptably) unpredictable
+		NSSet *expectedNewEvents = [NSSet setWithArray:@[event1, event4, expectedEvent2AfterMove, expectedEvent3AfterMove, event5]];
+		NSSet *eventsAfterMoving = [NSSet setWithArray:track.events];
+		XCTAssertEqualObjects(eventsAfterMoving, expectedNewEvents, @"Moving an event in MIKMIDITrack failed.");
+	}
+	[track removeObserver:self forKeyPath:@"events"];
+}
+
+- (void)testMovingEventsInARange
+{
+	MIKMIDISequence *sequence = [MIKMIDISequence sequence];
+	MIKMIDITrack *track = [sequence addTrack];
+	MIKMIDIEvent *event1 = [MIKMIDINoteEvent noteEventWithTimeStamp:1 note:60 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event2 = [MIKMIDINoteEvent noteEventWithTimeStamp:2 note:61 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event3 = [MIKMIDINoteEvent noteEventWithTimeStamp:3 note:62 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event4 = [MIKMIDINoteEvent noteEventWithTimeStamp:4 note:63 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event5 = [MIKMIDINoteEvent noteEventWithTimeStamp:6 note:64 velocity:127 duration:1 channel:0];
+	NSArray *allEvents = @[event1, event2, event3, event4, event5];
+	[track addEvents:allEvents];
+	
+	self.eventsChangeNotificationReceived = NO;
+	[track addObserver:self forKeyPath:@"events" options:0 context:NULL];
+	{
+		// Move event 2 to timestamp 5
+		[track moveEventsFromStartingTimeStamp:1.5 toEndingTimeStamp:3.5 byAmount:3];
+		XCTAssertTrue(self.eventsChangeNotificationReceived, @"Moving events in MIKMIDITrack did not produce a KVO notification.");
+		MIKMIDIEvent *expectedEvent2AfterMove = [MIKMIDINoteEvent noteEventWithTimeStamp:5 note:61 velocity:127 duration:1 channel:0];
+		MIKMIDIEvent *expectedEvent3AfterMove = [MIKMIDINoteEvent noteEventWithTimeStamp:6 note:62 velocity:127 duration:1 channel:0];
+		
+		// Use sets, because order of events with the same timestamp is (acceptably) unpredictable
+		NSSet *expectedNewEvents = [NSSet setWithArray:@[event1, event4, expectedEvent2AfterMove, expectedEvent3AfterMove, event5]];
+		NSSet *eventsAfterMoving = [NSSet setWithArray:track.events];
+		XCTAssertEqualObjects(eventsAfterMoving, expectedNewEvents, @"Moving an event in MIKMIDITrack failed.");
+	}
+	[track removeObserver:self forKeyPath:@"events"];
+}
+
+- (void)testMovingEventsPastTheEnd
+{
+	MIKMIDISequence *sequence = [MIKMIDISequence sequence];
+	MIKMIDITrack *track = [sequence addTrack];
+	MIKMIDIEvent *event1 = [MIKMIDINoteEvent noteEventWithTimeStamp:1 note:60 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event2 = [MIKMIDINoteEvent noteEventWithTimeStamp:2 note:61 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event3 = [MIKMIDINoteEvent noteEventWithTimeStamp:3 note:62 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event4 = [MIKMIDINoteEvent noteEventWithTimeStamp:4 note:63 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event5 = [MIKMIDINoteEvent noteEventWithTimeStamp:6 note:64 velocity:127 duration:1 channel:0];
+	NSArray *allEvents = @[event1, event2, event3, event4, event5];
+	[track addEvents:allEvents];
+	
+	self.eventsChangeNotificationReceived = NO;
+	[track addObserver:self forKeyPath:@"events" options:0 context:NULL];
+	{
+		// Move event 2 to timestamp 5
+		[track moveEventsFromStartingTimeStamp:5 toEndingTimeStamp:7 byAmount:3];
+		XCTAssertTrue(self.eventsChangeNotificationReceived, @"Moving events in MIKMIDITrack did not produce a KVO notification.");
+		MIKMIDIEvent *expectedEvent5AfterMove = [MIKMIDINoteEvent noteEventWithTimeStamp:9 note:64 velocity:127 duration:1 channel:0];
+		
+		NSArray *expectedNewEvents = @[event1, event2, event3, event4, expectedEvent5AfterMove];
+		NSArray *eventsAfterMoving = track.events;
+		XCTAssertEqualObjects(eventsAfterMoving, expectedNewEvents, @"Moving an event in MIKMIDITrack failed.");
+		
+		XCTAssertGreaterThanOrEqual(track.length, expectedEvent5AfterMove.timeStamp, @"Moving last event in track didn't properly update its length.");
+	}
+	[track removeObserver:self forKeyPath:@"events"];
+}
+
+- (void)testMovingEventBackwards
+{
+	MIKMIDISequence *sequence = [MIKMIDISequence sequence];
+	MIKMIDITrack *track = [sequence addTrack];
+	MIKMIDIEvent *event1 = [MIKMIDINoteEvent noteEventWithTimeStamp:1 note:60 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event2 = [MIKMIDINoteEvent noteEventWithTimeStamp:3 note:61 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event3 = [MIKMIDINoteEvent noteEventWithTimeStamp:4 note:62 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event4 = [MIKMIDINoteEvent noteEventWithTimeStamp:6 note:63 velocity:127 duration:1 channel:0];
+	NSArray *allEvents = @[event1, event2, event3, event4];
+	[track addEvents:allEvents];
+	
+	self.eventsChangeNotificationReceived = NO;
+	[track addObserver:self forKeyPath:@"events" options:0 context:NULL];
+	{
+		// Move event 2 to timestamp 5
+		[track moveEventsFromStartingTimeStamp:4 toEndingTimeStamp:4 byAmount:-2];
+		XCTAssertTrue(self.eventsChangeNotificationReceived, @"Moving an event in MIKMIDITrack did not produce a KVO notification.");
+		MIKMIDIEvent *expectedEvent3AfterMove = [MIKMIDINoteEvent noteEventWithTimeStamp:2 note:62 velocity:127 duration:1 channel:0];
+		NSArray *expectedNewEvents = @[event1, expectedEvent3AfterMove, event2, event4];
+		XCTAssertEqualObjects(track.events, expectedNewEvents, @"Moving an event in MIKMIDITrack failed.");
+	}
+	[track removeObserver:self forKeyPath:@"events"];
+}
+
+- (void)testMovingEventsInARangeBackwards
+{
+	MIKMIDISequence *sequence = [MIKMIDISequence sequence];
+	MIKMIDITrack *track = [sequence addTrack];
+	MIKMIDIEvent *event1 = [MIKMIDINoteEvent noteEventWithTimeStamp:1 note:60 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event2 = [MIKMIDINoteEvent noteEventWithTimeStamp:5 note:61 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event3 = [MIKMIDINoteEvent noteEventWithTimeStamp:6 note:62 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event4 = [MIKMIDINoteEvent noteEventWithTimeStamp:8 note:63 velocity:127 duration:1 channel:0];
+	MIKMIDIEvent *event5 = [MIKMIDINoteEvent noteEventWithTimeStamp:10 note:64 velocity:127 duration:1 channel:0];
+	NSArray *allEvents = @[event1, event2, event3, event4, event5];
+	[track addEvents:allEvents];
+	
+	self.eventsChangeNotificationReceived = NO;
+	[track addObserver:self forKeyPath:@"events" options:0 context:NULL];
+	{
+		// Move event 2 to timestamp 5
+		[track moveEventsFromStartingTimeStamp:5.5 toEndingTimeStamp:8.5 byAmount:-4];
+		XCTAssertTrue(self.eventsChangeNotificationReceived, @"Moving events in MIKMIDITrack did not produce a KVO notification.");
+		MIKMIDIEvent *expectedEvent3AfterMove = [MIKMIDINoteEvent noteEventWithTimeStamp:2 note:62 velocity:127 duration:1 channel:0];
+		MIKMIDIEvent *expectedEvent4AfterMove = [MIKMIDINoteEvent noteEventWithTimeStamp:4 note:63 velocity:127 duration:1 channel:0];
+		
+		// Use sets, because order of events with the same timestamp is (acceptably) unpredictable
+		NSArray *expectedNewEvents = @[event1, expectedEvent3AfterMove, expectedEvent4AfterMove, event2, event5];
+		NSArray *eventsAfterMoving = track.events;
+		XCTAssertEqualObjects(eventsAfterMoving, expectedNewEvents, @"Moving an event in MIKMIDITrack failed.");
+	}
+	[track removeObserver:self forKeyPath:@"events"];
+}
+
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
 	if ([object isKindOfClass:[MIKMIDITrack class]] && [keyPath isEqualToString:@"events"]) {
