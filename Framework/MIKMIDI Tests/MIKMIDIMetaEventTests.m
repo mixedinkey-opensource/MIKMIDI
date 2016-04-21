d6c969ccc3c43b7d57a36a501c3e29a3c86f5965
Andrew Madsen <andrew@mixedinkey.com>
2016-04-21 11:00:02 -0600

    Issue #150: Added customized initializer for MIKMIDIMetaTextEvent.

--- Framework/MIKMIDI Tests/MIKMIDIMetaEventTests.m
+++ Framework/MIKMIDI Tests/MIKMIDIMetaEventTests.m
@@ -24,6 +24,18 @@
 															 timeStamp:10];
 	XCTAssertTrue([textEvent isMemberOfClass:[MIKMIDIMetaTextEvent class]], @"MIKMIDIMetaEvent initializer didn't produce expected subclass (MIKMIDIMetaTextEvent)");
 	XCTAssertEqualObjects(textEvent.string, text, @"MIKMIDIMetaTextEvent didn't have expected string value.");
+	XCTAssertEqual(textEvent.metadataType, MIKMIDIMetaEventTypeTextEvent, "MIKMIDIMetaTextEvent didn't have expected metadataType.");
+	XCTAssertEqual(textEvent.timeStamp, 10, "MIKMIDIMetaTextEvent didn't have expected timeStamp.");
+}
+
+- (void)testMetaTextEventInit
+{
+	NSString *text = @"Mixed In Key";
+	MIKMIDIMetaTextEvent *textEvent = [[MIKMIDIMetaTextEvent alloc] initWithString:text timeStamp:27];
+	XCTAssertTrue([textEvent isMemberOfClass:[MIKMIDIMetaTextEvent class]], @"MIKMIDIMetaEvent initializer didn't produce expected subclass (MIKMIDIMetaTextEvent)");
+	XCTAssertEqualObjects(textEvent.string, text, @"MIKMIDIMetaTextEvent didn't have expected string value.");
+	XCTAssertEqual(textEvent.metadataType, MIKMIDIMetaEventTypeTextEvent, "MIKMIDIMetaTextEvent didn't have expected metadataType.");
+	XCTAssertEqual(textEvent.timeStamp, 27, "MIKMIDIMetaTextEvent didn't have expected timeStamp.");
 }
 
 @end
