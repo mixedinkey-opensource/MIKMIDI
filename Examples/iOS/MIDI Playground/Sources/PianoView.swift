02de6eec2b248ab56f81e2b06a94c0c5d5aaeb82
Andrew Madsen <andrew@openreelsoftware.com>
2017-11-29 11:03:55 -0700

    MIDI Playground: Simplify and improve PianoView

--- Examples/iOS/MIDI Playground/Sources/PianoView.swift
+++ Examples/iOS/MIDI Playground/Sources/PianoView.swift
@@ -11,114 +11,88 @@ import UIKit
 @IBDesignable class PianoView: UIView {
 	
 	override init(frame: CGRect) {
-		self.numberOfKeys = 128
-		self.numberOfBlackKeys = 53
-		self.numberOfWhiteKeys = 75
+		numberOfKeys = 128
+		numberOfWhiteKeys = 75
 		super.init(frame: frame)
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
-		self.numberOfKeys = 128
-		self.numberOfBlackKeys = 53
-		self.numberOfWhiteKeys = 75
+		numberOfKeys = 128
+		numberOfWhiteKeys = 75
 		super.init(coder: aDecoder)
 	}
 	
+	// MARK: Public
+	
 	// MARK: Drawing
 	
-	func drawBlackNote(_ offset: CGFloat) {
-		let offset = offset
-		let keyWidth = self.whiteKeyWidth * 0.7
-		
-		var rect = CGRect.zero
-		if self.isVertical {
-			rect = CGRect(x: 0, y: offset + keyWidth, width: self.bounds.width * 0.6, height: keyWidth).integral
-		} else {
-			rect = CGRect(x: offset + keyWidth, y: 0, width: keyWidth, height: self.bounds.height * 0.6).integral
+	private func draw(note: Int) {
+		var rect = rectForKey(note: note)
+		if !noteIsBlack(note) {
+			if isVertical {
+				rect.size.height = 1
+			} else {
+				rect.size.width = 1
+			}
 		}
-		
 		UIColor.black.setFill()
-		UIBezierPath(rect: rect).fill()
-	}
-	
-	func drawWhiteNote(_ offset: CGFloat) {
-		
-		var rect = CGRect.zero
-		if self.isVertical {
-			rect = CGRect(x: 0, y: offset, width: self.bounds.width, height: 1).integral
-		} else {
-			rect = CGRect(x: offset, y: 0, width: 1, height: self.bounds.height).integral
-		}
-		
-		UIColor.lightGray.setFill()
 		UIBezierPath(rect: rect).fill()
 	}
 	
 	override func draw(_ rect: CGRect) {
 		UIColor.white.setFill()
 		UIColor.black.setStroke()
-		UIBezierPath(rect: self.bounds).fill()
-		UIBezierPath(rect: self.bounds).stroke()
+		UIBezierPath(rect: bounds).fill()
+		UIBezierPath(rect: bounds).stroke()
 		
-		var offset: CGFloat = 0
-		
-//		UIColor.grayColor().setFill()
-//		UIBezierPath(rect: CGRectMake(offset+self.whiteKeyWidth, 0, 1, CGRectGetHeight(self.bounds))).fill()
-		
-		let numKeys = self.numberOfKeys
-		for noteNumber in 0..<numKeys {
-			if !noteIsBlack(noteNumber) {
-				drawWhiteNote(offset)
-				offset += self.whiteKeyWidth
-			}
-		}
-		
-		offset = 0
-		for noteNumber in 0..<numKeys {
-			if (noteIsBlack(noteNumber)) {
-				drawBlackNote(offset)
-			} else {
-				offset += self.whiteKeyWidth
-			}
-		}
-		
-//		UIColor.grayColor().setFill()
-//		UIBezierPath(rect: CGRectMake(offset+self.whiteKeyWidth, 0, 1, CGRectGetHeight(self.bounds))).fill()
+		Array(0..<numberOfKeys).forEach(draw)
 	}
 	
 	// MARK: Private Utilities
 	
-	fileprivate func noteIsBlack(_ noteNumber: Int) -> Bool {
-		return NSSet(array:[1, 3, 6, 8, 10]).contains((noteNumber % 12));
+	private func noteIsBlack(_ noteNumber: Int) -> Bool {
+		return [1, 3, 6, 8, 10].contains(noteNumber % 12);
+	}
+	
+	private func rectForKey(note: Int) -> CGRect {
+		var keyWidth = whiteKeyWidth
+		var keyHeight = isVertical ? bounds.width : bounds.height
+		if noteIsBlack(note) {
+			keyWidth *= 0.7
+			keyHeight *= 0.6
+		}
+		
+		let whiteNotes = Array(0..<note).filter { !noteIsBlack($0) }
+		let offset = CGFloat(whiteNotes.count) * whiteKeyWidth
+		
+		var rect = CGRect.zero
+		if isVertical {
+			rect = CGRect(x: 0, y: offset + keyWidth, width: keyHeight, height: keyWidth)
+		} else {
+			rect = CGRect(x: offset + keyWidth, y: 0, width: keyWidth, height: keyHeight)
+		}
+		return rect.integral
 	}
 	
 	// MARK: Properties
 	
 	var isVertical: Bool {
-		return self.bounds.width < self.bounds.height
+		return bounds.width < bounds.height
 	}
 	
 	@IBInspectable var numberOfKeys: Int {
 		didSet {
-			var numBlackKeys = 0
-			for index in 0..<numberOfKeys {
-				if noteIsBlack(index) {
-					numBlackKeys += 1
-				}
-			}
+			let blackKeys = Array(0..<numberOfKeys).filter(noteIsBlack)
+			numberOfWhiteKeys = numberOfKeys - blackKeys.count
 			
-			self.numberOfBlackKeys = numBlackKeys
-			self.numberOfWhiteKeys = numberOfKeys - numBlackKeys
-			
-			self.setNeedsDisplay()
+			setNeedsDisplay()
 		}
 	}
 	
 	var numberOfWhiteKeys: Int
-	var numberOfBlackKeys: Int
 	
 	var whiteKeyWidth: CGFloat {
-		let width = self.isVertical ? self.bounds.height : self.bounds.width
-		return width / CGFloat(self.numberOfWhiteKeys)
+		let width = isVertical ? bounds.height : bounds.width
+		return width / CGFloat(numberOfWhiteKeys)
 	}
 }
