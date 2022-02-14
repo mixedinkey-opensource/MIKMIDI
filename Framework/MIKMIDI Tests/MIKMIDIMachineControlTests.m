d09d38dc210a296eb2f3f20a688324d5b7949f9f
Andrew Madsen <andrew@openreelsoftware.com>
2022-02-13 18:18:58 -0700

    Issue #327: Flesh out implementation of MIKMIDIMachineControlCommand including tests

--- Framework/MIKMIDI Tests/MIKMIDIMachineControlTests.m
+++ Framework/MIKMIDI Tests/MIKMIDIMachineControlTests.m
@@ -17,11 +17,51 @@
 
 - (void)testGenericMIDIMachineControlCommand
 {
-    NSArray *bytes = @[@(0xf0), @(0x7f), @(0x7f), @(0x06), @(0x01)];
+    Class immutableClass = [MIKMIDIMachineControlCommand class];
+    Class mutableClass = [MIKMutableMIDIMachineControlCommand class];
+
+    NSArray *bytes = @[@(0xf0), @(0x7f), @(0xab), @(0x07), @(0x01)];
     MIDIPacket packet = MIKMIDIPacketCreate(0, bytes.count, bytes);
 
-    MIKMIDICommand *command = [MIKMIDICommand commandWithMIDIPacket:&packet];
+    MIKMIDIMachineControlCommand *command = [MIKMIDIMachineControlCommand commandWithMIDIPacket:&packet];
     XCTAssertTrue([command isMemberOfClass:[MIKMIDIMachineControlCommand class]]);
+    XCTAssertEqual(command.deviceAddress, 0xab);
+    XCTAssertEqual(command.direction, MIKMIDIMachineControlDirectionResponse);
+    XCTAssertEqual(command.MMCCommandType, MIKMIDIMachineControlCommandTypeStop);
+    XCTAssertEqual(command.commandType, MIKMIDICommandTypeSystemExclusive, @"[[MIKMIDIMachineControlCommand alloc] init] produced a command instance with the wrong command type.");
+    XCTAssert([[command copy] isMemberOfClass:immutableClass], @"[MIKMIDIMachineControlCommand copy] did not return an MIKMIDIMachineControlCommand instance.");
+    XCTAssert([[command mutableCopy] isMemberOfClass:mutableClass], @"-[MIKMIDIMachineControlCommand mutableCopy] did not return a mutableClass instance.");
+
+
+    MIKMutableMIDIMachineControlCommand *mutableCommand = [[MIKMutableMIDIMachineControlCommand alloc] init];
+    XCTAssert([mutableCommand isMemberOfClass:mutableClass], @"-[MIKMIDIMachineControlCommand mutableCopy] did not return a mutableClass instance.");
+    XCTAssert([[mutableCommand copy] isMemberOfClass:immutableClass], @"[MIKMutableMIDIMachineControlCommand copy] did not return an MIKMIDIMachineControlCommand instance.");
+    XCTAssertEqual(mutableCommand.commandType, MIKMIDICommandTypeSystemExclusive, @"[[MIKMIDIMachineControlCommand alloc] init] produced a command instance with the wrong command type.");
+
+    XCTAssertEqual(mutableCommand.deviceAddress, 0x7f);
+    XCTAssertEqual(mutableCommand.direction, MIKMIDIMachineControlDirectionCommand);
+    XCTAssertEqual(mutableCommand.MMCCommandType, MIKMIDIMachineControlCommandTypeUnknown);
+
+    XCTAssertThrows([(MIKMutableMIDIMachineControlCommand *)command setDirection:MIKMIDIMachineControlDirectionResponse]);
+    XCTAssertThrows([(MIKMutableMIDIMachineControlCommand *)command setMMCCommandType:MIKMIDIMachineControlCommandTypePlay]);
+
+    XCTAssertNoThrow([mutableCommand setDirection:MIKMIDIMachineControlDirectionResponse]);
+    XCTAssertNoThrow([mutableCommand setMMCCommandType:MIKMIDIMachineControlCommandTypePlay]);
+
+    mutableCommand.deviceAddress = 0x9f;
+    mutableCommand.direction = MIKMIDIMachineControlDirectionResponse;
+    mutableCommand.MMCCommandType = MIKMIDIMachineControlCommandTypeRecordExit;
+    XCTAssertEqual(mutableCommand.deviceAddress, 0x9f);
+    XCTAssertEqual(mutableCommand.direction, MIKMIDIMachineControlDirectionResponse);
+    XCTAssertEqual(mutableCommand.MMCCommandType, MIKMIDIMachineControlCommandTypeRecordExit);
+
+    MIKMIDIMachineControlCommand *createdCommand =
+    [MIKMIDIMachineControlCommand machineControlCommandWithDeviceAddress:0xcd
+                                                               direction:MIKMIDIMachineControlDirectionResponse
+                                                          MMCCommandType:MIKMIDIMachineControlCommandTypePause];
+    XCTAssertEqual(createdCommand.deviceAddress, 0xcd);
+    XCTAssertEqual(createdCommand.direction, MIKMIDIMachineControlDirectionResponse);
+    XCTAssertEqual(createdCommand.MMCCommandType, MIKMIDIMachineControlCommandTypePause);
 }
 
 - (void)testMMCLocateTargetCommand
