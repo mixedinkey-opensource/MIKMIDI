e12f06a5022f66d198fe05de8be8f53d1e07a792
Andrew Madsen <andrew@openreelsoftware.com>
2022-06-19 23:46:17 -0600

    Fix warning about TARGET_OS_IPHONE being undefined when building with SPM

--- Source/MIKMIDIMacDebugQuickLookSupport.m
+++ Source/MIKMIDIMacDebugQuickLookSupport.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Mixed In Key. All rights reserved.
 //
 
+#import <TargetConditionals.h>
+
 #if !TARGET_OS_IPHONE
 
 #import <CoreMIDI/CoreMIDI.h>
--- Source/MIKMIDIMappingManager.m
+++ Source/MIKMIDIMappingManager.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Mixed In Key. All rights reserved.
 //
 
+#import <TargetConditionals.h>
+
 #if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
 #else
