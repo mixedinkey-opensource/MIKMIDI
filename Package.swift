d684446698d3e34ae0493bf9b51796af26618652
Andrew Madsen <andrew@openreelsoftware.com>
2026-04-07 23:36:45 -0600

    Bump deployment to macOS 11 and iOS 15 in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "MIKMIDI",
     platforms: [
-        .macOS(.v10_10),
-        .iOS(.v12)
+        .macOS(.v11),
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
